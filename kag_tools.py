5f34f9c95a8c375c22761007a0134cda716fd1bb
DevDodge <developerdodge@gmail.com>
2025-04-19 18:03:12 +0200

    Refactor import statement for clarity.
    Added a comment to the `WebDriverWait` import for better readability and code maintainability. No functional changes were made to the file.

--- kag_tools.py
+++ kag_tools.py
@@ -12,7 +12,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait #comment
 from selenium.webdriver.support import expected_conditions as EC
 from openpyxl import Workbook, load_workbook
 import json
